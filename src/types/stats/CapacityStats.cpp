c9c99bbea3a1cac169a0edb47755c9a1178010d7
Cooper Larson <cooper.larson1@gmail.com>
2026-01-25 13:09:09 -0700

    capacity stats uses uintmax_t to json

--- src/types/stats/CapacityStats.cpp
+++ src/types/stats/CapacityStats.cpp
@@ -10,8 +10,6 @@
 
 #include <nlohmann/json.hpp>
 
-#include "util/files.hpp"
-
 using namespace vh::types;
 using namespace vh::storage;
 using namespace vh::database;
@@ -32,6 +30,12 @@ toTop10Array(const std::vector<ExtensionStat>& v) {
     return a;
 }
 
+static nlohmann::json top10ToDict(const std::array<std::pair<std::string, uintmax_t>, 10>& arr) {
+    nlohmann::json j;
+    for (const auto & [k, v] : arr) j[k] = v;
+    return j;
+}
+
 CapacityStats::CapacityStats(const unsigned int vaultId) {
     const auto engine = services::ServiceDepsRegistry::instance().storageManager->getEngine(vaultId);
     if (!engine) throw std::runtime_error("vaultId not available");
@@ -46,7 +50,7 @@ CapacityStats::CapacityStats(const unsigned int vaultId) {
     free_space = engine->freeSpace();
     file_count = dir->file_count;
     directory_count = dir->subdirectory_count;
-    average_file_size_bytes = dir->size_bytes / file_count;
+    average_file_size_bytes = file_count > 0 ? dir->size_bytes / file_count : 0;
     if (const auto largest = FileQueries::getLargestFile(engine->vault->id))
         largest_file_size_bytes = largest->size_bytes;
     top_file_extensions_by_size =
@@ -54,14 +58,14 @@ CapacityStats::CapacityStats(const unsigned int vaultId) {
 }
 
 void vh::types::to_json(nlohmann::json& j, const std::shared_ptr<CapacityStats>& stats) {
-    j["capacity"] = bytesToSize(stats->capacity);
-    j["logical_size"] = bytesToSize(stats->logical_size);
-    j["physical_size"] = bytesToSize(stats->physical_size);
-    j["free_space"] = bytesToSize(stats->free_space);
-    j["cache_size"] = bytesToSize(stats->cache_size);
+    j["capacity"] = stats->capacity;
+    j["logical_size"] = stats->logical_size;
+    j["physical_size"] = stats->physical_size;
+    j["free_space"] = stats->free_space;
+    j["cache_size"] = stats->cache_size;
     j["file_count"] = stats->file_count;
     j["directory_count"] = stats->directory_count;
-    j["average_file_size"] = bytesToSize(stats->average_file_size_bytes);
-    j["largest_file_size"] = bytesToSize(stats->largest_file_size_bytes);
-    j["top_file_extensions"] = stats->top_file_extensions_by_size;
+    j["average_file_size"] = stats->average_file_size_bytes;
+    j["largest_file_size"] = stats->largest_file_size_bytes;
+    j["top_file_extensions"] = top10ToDict(stats->top_file_extensions_by_size);
 }

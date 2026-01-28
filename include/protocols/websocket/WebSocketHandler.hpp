dbfea77ae6fad5a1eb24e43e12235358dd9d59ec
Cooper Larson <cooper.larson1@gmail.com>
2026-01-28 13:06:18 -0700

    rm unused template arg and imports

--- include/protocols/websocket/WebSocketHandler.hpp
+++ include/protocols/websocket/WebSocketHandler.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include "protocols/websocket/WebSocketSession.hpp"
-#include "types/User.hpp"
-
 #include <memory>
 
 namespace vh::websocket {
@@ -27,24 +24,6 @@ class WebSocketHandler {
     void registerSettingsHandlers() const;
     void registerGroupHandlers() const;
     void registerStatHandlers() const;
-
-    template <typename... Funcs> static void enforcePermissions(
-        WebSocketSession& session,
-        const unsigned int vaultId,
-        const std::filesystem::path& path, // Add path param
-        Funcs... checks) {
-        const auto user = session.getAuthenticatedUser();
-        if (!user) throw std::runtime_error("Unauthorized");
-
-        if (user->isAdmin()) return;
-
-        const auto role = user->getRole(vaultId);
-        if (!role) throw std::runtime_error("No role assigned for this vault/volume");
-
-        if (!( ((role.get()->*checks)(path)) || ... )) {
-            throw std::runtime_error("Permission denied: Required permission not granted");
-        }
-    }
 };
 
-} // namespace vh::websocket
+}

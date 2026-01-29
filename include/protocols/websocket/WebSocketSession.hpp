01bfa44b0f57d80f5ab159e815b803c143c5ea94
Cooper Larson <cooper.larson1@gmail.com>
2026-01-29 09:10:16 -0700

    refactor WebSocketSession - simplify

--- include/protocols/websocket/WebSocketSession.hpp
+++ include/protocols/websocket/WebSocketSession.hpp
@@ -4,15 +4,13 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/websocket.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
+
+#include <atomic>
+#include <deque>
 #include <memory>
 #include <nlohmann/json.hpp>
-#include <queue>
-#include <unordered_set>
+#include <string>
 #include <fstream>
-#include <atomic>
 
 struct UploadContext {
     std::string path;
@@ -23,11 +21,6 @@ struct UploadContext {
 
 using RequestType = boost::beast::http::request<boost::beast::http::string_body>;
 
-namespace vh::auth {
-class SessionManager;
-class AuthManager;
-} // namespace vh::auth
-
 namespace vh::types {
 struct User;
 }
@@ -36,85 +29,79 @@ namespace vh::websocket {
 class WebSocketRouter;
 class UploadHandler;
 
-namespace beast = boost::beast;
+namespace beast     = boost::beast;
 namespace websocket = beast::websocket;
-namespace asio = boost::asio;
-using tcp = asio::ip::tcp;
-using json = nlohmann::json;
+namespace asio      = boost::asio;
+using tcp           = asio::ip::tcp;
+using json          = nlohmann::json;
 
 class WebSocketSession : public std::enable_shared_from_this<WebSocketSession> {
 public:
+    explicit WebSocketSession(const std::shared_ptr<WebSocketRouter>& router);
     ~WebSocketSession();
 
-    explicit WebSocketSession(const std::shared_ptr<WebSocketRouter>& router);
-
-    void send(const json& message);
-
     void accept(tcp::socket&& socket);
+    void send(const json& message);
+    void close();
 
-    void requestClose();
-
-    void subscribeChannel(const std::string& channel);
-
-    void unsubscribeChannel(const std::string& channel);
-
-    bool isSubscribedTo(const std::string& channel);
-
-    std::unordered_set<std::string> getSubscribedChannels();
-
-    const std::string& getUUID() const { return uuid; }
-
-    // Accessors for session state
-    std::shared_ptr<types::User> getAuthenticatedUser() const;
+    const std::string& getUUID() const { return uuid_; }
 
     void setAuthenticatedUser(std::shared_ptr<types::User> user);
-
     void setRefreshTokenCookie(const std::string& token);
-
     void setHandshakeRequest(const RequestType& req);
 
+    std::shared_ptr<types::User> getAuthenticatedUser() const;
     std::string getClientIp() const;
     std::string getUserAgent() const;
     std::string getRefreshToken() const;
+
     std::shared_ptr<UploadHandler> getUploadHandler() const { return uploadHandler_; }
 
-    static std::string generateUUIDv4() {
-        static boost::uuids::random_generator generator;
-        const boost::uuids::uuid uuid = generator();
-        return boost::uuids::to_string(uuid);
-    }
+    static std::string generateUUIDv4();
 
 private:
-    std::shared_ptr<auth::AuthManager> authManager_;
-    const std::string uuid = generateUUIDv4();
-    std::shared_ptr<websocket::stream<tcp::socket> > ws_;
-    beast::flat_buffer buffer_{8192}, tmpBuffer_{4096};
-    asio::any_io_executor strand_;
-    RequestType handshakeRequest_;
-    std::shared_ptr<UploadHandler> uploadHandler_;
+    // ---- lifecycle / io
+    void startReadLoop();
+    void doRead();
+    void onRead(const beast::error_code& ec, std::size_t bytesRead);
 
+    void maybeStartWrite();
+    void doWrite();
+    void onWrite(const beast::error_code& ec, std::size_t bytesWritten);
+
+    // ---- handshake helpers
+    void onHeadersRead(const std::shared_ptr<RequestType>& req, const beast::error_code& ec, std::size_t bytesRead);
+    void onHandshakeAccepted(const beast::error_code& ec);
+    void hydrateFromRequest(const RequestType& req);
+    void installHandshakeDecorator() const;
+
+    // ---- misc helpers
+    static void logFail(std::string_view where, const beast::error_code& ec);
+    void sendParseError(std::string_view msg);
+    void sendInternalError();
+
+private:
+    const std::string uuid_{generateUUIDv4()};
+
+    std::shared_ptr<websocket::stream<tcp::socket>> ws_;
+    asio::any_io_executor strand_;
+
+    beast::flat_buffer buffer_{8192};
+    beast::flat_buffer tmpBuffer_{4096}; // used during HTTP header read/handshake
+    RequestType handshakeRequest_;
+
+    std::shared_ptr<UploadHandler> uploadHandler_;
     std::shared_ptr<WebSocketRouter> router_;
     std::shared_ptr<types::User> authenticatedUser_;
+
     std::string refreshToken_;
     std::string userAgent_;
     std::string ipAddress_;
+
     std::atomic_bool closing_{false};
 
-    std::queue<std::string> writeQueue_;
-    bool writingInProgress_ = false;
-
-    std::unordered_set<std::string> subscribedChannels_;
-    bool isRegistered_ = false;
-
-    void doRead();
-
-    void onRead(beast::error_code ec, std::size_t bytesTransferred);
-
-    void doWrite();
-
-    void onWrite(beast::error_code ec, std::size_t bytesTransferred);
-
-    void close();
+    bool writing_ = false;                 // only touched on strand
+    std::deque<std::string> writeQueue_;   // only touched on strand
 };
 
 } // namespace vh::websocket
